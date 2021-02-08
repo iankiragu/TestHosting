f28ba75a4c4573a6e1bec0dc8baec7251031ceb1
iankiragu <iankiragu63@gmail.com>
2021-02-08 20:49:03 +0300

    Seeder changes

--- database/seeds/SpecializationsSeeder.php
+++ database/seeds/SpecializationsSeeder.php
@@ -12,17 +12,12 @@ class SpecializationsSeeder extends Seeder
     public function run()
     {
         DB::table('specializations')->insert([
-            ['name'=>'Neurology'],
-            ['name'=>'Gerontologist'],
-            ['name'=>'Obstetrics & Gynaecology'],
-            ['name'=>'Anaesthesia & Critical Care'],
-            ['name'=>'Emergency Medicine'],
-            ['name'=>'Internal Medicine'],
-            ['name'=>'Cardiology'],
-            ['name'=>'Oncology, Urology'],
-            ['name'=>'Dental Sciences'],
-            ['name'=>'Neurosurgery'],
-            ['name'=>'Pathology'],
+            ['name'=>'Psychologist'],
+            ['name'=>'Psychiatrist'],
+            ['name'=>'Psychoanalyst'],
+            ['name'=>'Mental Health Counsellor'],
+            ['name'=>'Psychotherapist'],
+
         ]);
     }
 }
